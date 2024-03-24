57c5a59b7fd5823ddf05042c231a56e72f763588
Ashish Kumar <Ashishsarraf7@gmail.com>
2024-03-24 15:33:02 +0530

    Project init

---
+++ Models/Auth/AuthUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PunjabOrnaments.Common.Models.Auth
+{
+    public class AuthUser
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        //public string UserName { get; set; }
+        //public string Password { get; set; }
+        public string Token { get; set; }
+        public string Device { get; set; } = "Mobile";
+
+    }
+}
---
+++ Models/Enum/ApprovalEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace PunjabOrnaments.Common.Models.Enum
+{
+    public enum ApprovalEnum
+    {
+        Approval = 0,
+        Reject = 1,
+        OnHold = 2,
+    }
+}
