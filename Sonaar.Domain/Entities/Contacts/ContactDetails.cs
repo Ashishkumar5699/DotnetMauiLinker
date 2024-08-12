ceed1d3ac22f2607d06ae2d81af494e5a000168f
Ashish Kumar <Ashishsarraf7@gmail.com>
2024-08-12 18:18:23 +0530

    Contact Profile added

---
+++ Sonaar.Domain/Entities/Contacts/ContactDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Sonaar.Domain.Dto.CustomerDirectory;
+
+namespace Sonaar.Domain.Entities.Contacts
+{
+	public class ContactDetails : ConsumerDTO
+    {
+        [Key]
+        public int ContactId { get; set; }
+    }
+}
+
---
+++ Sonaar.Domain/Mapper/ContactProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using Sonaar.Domain.Dto.CustomerDirectory;
+using Sonaar.Domain.Entities.Contacts;
+
+namespace Sonaar.Domain.Mapper
+{
+	public class ContactProfile : Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<ConsumerDTO, ContactDetails>();
+        }
+	}
+}
