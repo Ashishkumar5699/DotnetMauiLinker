3585d1be4ec9b3725974796510ac2630b650d787
Ashish <ashishsarraf7@gmail.com>
2024-11-26 10:33:40 +0530

    update product model

--- Sonaar.Domain/Models/Bills/ProductModel.cs
+++ Sonaar.Domain/Models/Bills/ProductModel.cs
@@ -9,33 +9,33 @@ namespace Sonaar.Domain.Bills
 
         public string Purity { get; set; }
 
-        private decimal _weight; 
-        public decimal Weight 
-        {
-            get => Math.Round(_weight,2);
-            set => _weight = Math.Round(_weight, 0);
-        }
+        // private decimal _weight; 
+        public decimal Weight { get; set; }
+        // {
+        //     get => Math.Round(_weight,2);
+        //     set => _weight = Math.Round(_weight, 0);
+        // }
 
-        private decimal _rate; 
-        public decimal Rate 
-        { 
-            get => Math.Round(_rate,2);
-            set => _weight = Math.Round(_rate, 0);
-        }
+        // private decimal _rate; 
+        public decimal Rate { get; set; }
+        // { 
+        //     get => Math.Round(_rate,2);
+        //     set => _weight = Math.Round(_rate, 0);
+        // }
 
-        private decimal _making_Charge; 
-        public decimal Making_Charge 
-        { 
-            get => Math.Round(_making_Charge,2);
-            set => _weight = Math.Round(_making_Charge, 0);
-        }
+        // private decimal _making_Charge; 
+        public decimal Making_Charge { get; set; }
+        // { 
+        //     get => Math.Round(_making_Charge,2);
+        //     set => _weight = Math.Round(_making_Charge, 0);
+        // }
 
-        private decimal _amount;
-        public decimal Amount 
-        { 
-            get => Math.Round(_amount,2);
-            set => _weight = Math.Round(_amount, 0);
-        }
+        // private decimal _amount;
+        public decimal Amount { get; set; }
+        // { 
+        //     get => Math.Round(_amount,2);
+        //     set => _weight = Math.Round(_amount, 0);
+        // }
     }
 }
 
