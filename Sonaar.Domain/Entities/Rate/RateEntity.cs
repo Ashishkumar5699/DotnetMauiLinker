00941d4fc0b7fa2a01a64deeccf21c51f48a0265
Ashish <ashishsarraf7@gmail.com>
2024-12-25 16:37:17 +0530

    Rate Entity Added

--- Sonaar.Domain/Entities/Rate/RateEntity.cs
+++ Sonaar.Domain/Entities/Rate/RateEntity.cs
@@ -1,27 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Sonaar.Domain.Dto.Rate;
 using Sonaar.Domain.Entities.Authentication;
 
 namespace Sonaar.Domain.Entities.Rate;
 
-public class RateEntity
+public class RateEntity : RateDto
 {
-    public decimal Gold24K { get; set; } 
-
-    public decimal Gold22K { get; set; } 
-
-    public decimal Gold18K { get; set; } 
-    
-    public decimal Gold14K { get; set; }     
-    
-    public decimal Silver70 { get; set; }
-
-    public decimal Silver925 { get; set; } 
-
-    [ForeignKey("Id")]
-    public required AppUser AddedBy { get; set; }
-
-    public DateTime AddedOn { get; set; } 
-
-    public DateTime Expireon { get; set; } 
+    [Key]
+    public int RateId { get; set; }
 }
