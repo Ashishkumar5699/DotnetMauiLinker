2487e32a5ab6a1e9800f110c615b2e196728fa46
Ashish Kumar <ashishsarraf7@gmail.com>
2024-12-26 17:21:56 +0530

    rateDto and APi end point added

---
+++ Sonaar.Domain/Dto/Rate/RateDto.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Sonaar.Domain.Dto.Rate;
+
+public class RateDto
+{
+    public decimal Gold24K { get; set; } 
+
+    public decimal Gold22K { get; set; } 
+
+    public decimal Gold18K { get; set; } 
+
+    public decimal Gold14K { get; set; }     
+
+    public decimal Silver70 { get; set; }
+
+    public decimal Silver925 { get; set; } 
+
+    public DateTime AddedOn { get; set; } 
+
+    public DateTime Expireon { get; set; } 
+}
